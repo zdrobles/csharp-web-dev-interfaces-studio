402c07f8f1e360d084c17559695c2a7d7aeda3f1
Sally Steuterman <sally.steuterman@gmail.com>
2020-03-23 14:03:46 -0500

    Initial commit

---
+++ SpinningDiscs/CD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SpinningDiscs
+{
+    public class CD
+    {
+        // TODO: Implement your custom interface.
+
+        // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
+        //  need to be declared separately. 
+    }
+}
---
+++ SpinningDiscs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SpinningDiscs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Declare and initialize a CD and a DVD object.
+
+            // TODO: Call each CD and DVD method to verify that they work as expected.
+        }
+    }
+}
